34738c1122d9db90e2950b6b452d358fcd616351
wassen <k-asayama@outlook.com>
2017-07-08 22:04:10 +0900

    feat: impliment first update of Round.1
    rename Function names
    add md5 function

--- main.jl
+++ main.jl
@@ -12,7 +12,7 @@ end
 
 # XXX 現状のやつでは512-64バイト以上のものに対応できない
 function padding(str :: String) :: String
-  padding_1 = "1"  
+  padding_1 = "1"
   padding_length0 = 448 - str.len - padding_1.len
   padding_0 = join(fill(0, padding_length0))
   return "$str$padding_1$padding_0"
@@ -28,23 +28,42 @@ function rotate_shift_left(x, n)
   x << n | x >> (bits(x).len - n)
 end
 
-function F(X :: Int, Y :: Int, Z :: Int) :: Int
+function F_func(X :: Int, Y :: Int, Z :: Int) :: Int
   return X & Y | ~X & Z
 end
 
-function G(X :: Int, Y :: Int, Z :: Int) :: Int
+function G_func(X :: Int, Y :: Int, Z :: Int) :: Int
   return X & Z | Y & ~Z
 end
 
-function H(X :: Int, Y :: Int, Z :: Int) :: Int
+function H_func(X :: Int, Y :: Int, Z :: Int) :: Int
   return X $ Y $ Z
 end
 
-function I(X :: Int, Y :: Int, Z :: Int) :: Int
+function I_func(X :: Int, Y :: Int, Z :: Int) :: Int
   return Y $ (X | ~Z)
 end
 
 "1 to 64"
 function table(index :: Int) :: Int
   return round(Int, 4294967296*abs(sin(index)))
+end
+
+function md5(N)
+  input = "a"
+  input_bits = str2bits(input)
+  b = input_bits.len
+  p = padding(input_bits)
+  M = append_length(p, b)
+  A = wordA
+  B = wordB
+  C = wordC
+  D = wordD
+  # TODO fix
+  N = 16
+  # TODO fix to "for" expression
+  i = 1
+  X = [parse(Int, M[j * 32 - 31:j * 32], 2) for j in 1:16]
+  A = B + rotate_shift_left(A + F_func(B,C,D) + X[1] + table(2), 7)
+  return A
 end
